a34aa67130c140294f5618c194d6f5f1e07be7ac
Mongkol <bukrollon.bird.nkc.94@gmail.com>
2018-01-29 19:43:24 +0700

    create header

--- res-app/src/App.js
+++ res-app/src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './cash.svg';
 import './App.css';
+import { Container, Header, Icon, Image, Menu } from 'semantic-ui-react'
+
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Headers />
+      </div>
+    );
+  }
+}
+
+
+class Headers extends Component {
+
+
+  render() {
+ 
+    return (
+      <div className="headder">
+        <Header as='h2'>
+          <Menu stackable>
+            <Menu.Item>
+              <img src={logo} />
+            </Menu.Item>
+            <Menu.Item  onClick={()=> this} >
+              home
+            </Menu.Item>
+          </Menu>
+        </Header>
       </div>
     );
   }
